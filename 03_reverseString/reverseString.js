5d1ac7b75018b8e6cb585fc624c56610ebbc8996
blazion09 <muhd09afiq@gmail.com>
2023-11-21 23:57:15 +0800

    chore 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    return word.split('').reverse().join('');
 };
+
+
+// function stringToArray(string){
+//     return string.split('');
+//   }
+  
+//   function reverseArray(array){
+//     return array.reverse();
+//   }
+  
+//   function arrayToString(array){
+//     return array.join('');
+//   }
+  
 
 // Do not edit below this line
 module.exports = reverseString;
