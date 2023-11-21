b70f5da3dfda037e4a174faf70f6cc4b7f56e043
blazion09 <muhd09afiq@gmail.com>
2023-11-21 14:26:44 +0800

    finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let i = 0;
+    let string = '';
+    if (num < 0){
+        return 'ERROR';
+    } else {
+    while (i < num){
+        string += word;
+        i++;
+    }
+    return string;
+}
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
