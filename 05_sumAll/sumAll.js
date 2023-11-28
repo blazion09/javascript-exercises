c7d1bbddc23aa16955962a5711c6e18d4c709a67
blazion09 <muhd09afiq@gmail.com>
2023-11-28 15:55:15 +0800

    chore retry 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,29 @@
-const sumAll = function(input1, input2) {
-    if (input1 > input2){
-        let temp = input1;
-        input1 = input2;
-        input2 = temp;
-    }
-    if (input1 < 0 || input2 < 0){
-        return 'ERROR';
-    }
-    if (typeof(input1, input2) != "number"){
-        return 'ERROR';
-    }
-    let sum = input1;
-    for (let i = input1; i < input2; i++){
-        sum = sum + i + 1;
+// const sumAll = function(input1, input2) {
+//     if (input1 > input2){
+//         let temp = input1;
+//         input1 = input2;
+//         input2 = temp;
+//     }
+//     if (input1 < 0 || input2 < 0){
+//         return 'ERROR';
+//     }
+//     if (typeof(input1, input2) != "number"){
+//         return 'ERROR';
+//     }
+//     let sum = input1;
+//     for (let i = input1; i < input2; i++){
+//         sum = sum + i + 1;
+//     }
+//     return sum;
+// };
+const sumAll = function(...input) {
+    let sum = 0;
+    for (let arg of input){
+        sum += arg;
     }
     return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
