ba34c8a56e26f23c86bfa1b9737d4ff14ed6b0b5
blazion09 <muhd09afiq@gmail.com>
2023-11-29 11:15:03 +0800

    complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
 const convertToCelsius = function(input) {
-//Convert Celcius to Fahrenheit where x is in Celcius: (x − 32) × (5/9) °C
+//Convert to Celcius where x is in Fahrenheit: (x − 32) × (5/9) °C
   let rawNumber = ((input - 32) * (5/9));
   const convertedToOneDecimal = (Math.round(rawNumber * 10)) / 10;
   return convertedToOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+//Convert to Fahrenheit where x is in Celcius(x × 9/5 + 32) °F
+let rawNumber = (input * (9/5)) + 32;
+const convertedToOneDecimal = (Math.round(rawNumber * 10)) / 10;
+return convertedToOneDecimal;
 };
 
 // Do not edit below this line
