c6a5cdd33246bf2ad0f7c4fa3d5790ea69ecac4e
blazion09 <muhd09afiq@gmail.com>
2024-01-04 11:01:35 +0800

    complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const filteredWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedWord = filteredWord.split('').reverse().join('');
+    return filteredWord === reversedWord;
 };
-
+// /[^a-z0-9]/g
 // Do not edit below this line
 module.exports = palindromes;
