cdfc2439bdc33038975471ade6cffe538650a825
blazion09 <muhd09afiq@gmail.com>
2024-01-04 13:25:41 +0800

    complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if(n < 0){
+        return 'OOPS';
+    }
+    let fib = [1, 1];
+    n -= 1;
+    let nextnumber = 0;
+    for (let i = 0; i <= n; i++) {
+        nextNumber = fib[i] + fib[i + 1];
+        fib.push(nextNumber);
+    }
+    return fib[n];
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = fibonacci;
