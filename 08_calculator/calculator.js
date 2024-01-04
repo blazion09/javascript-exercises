e664fe2042b2c24c4212426eec1a5ea43cba5f92
blazion09 <muhd09afiq@gmail.com>
2024-01-04 10:10:49 +0800

    complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,19 +7,28 @@ const subtract = function(a,b) {
 };
 
 const sum = function(arr) {
-	return arr.join
+  return arr.reduce((total,current) => (total + current), 0)
 };
 
-const multiply = function(a,b) {
-  return a * b;
+const multiply = function(arr) {
+  return arr.reduce((initial, total) => initial * total);
 };
 
-const power = function() {
-	
+const power = function(int, power) {
+  return int ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(int) {
+	const factor = [];
+  if(int != 0){
+    for(let i = 1; i <= int; i++){
+      factor.push(i)
+    }
+  } else {
+    return 1;
+  }
+  
+  return factor.reduce((initial, total) => initial * total);
 };
 
 // Do not edit below this line
