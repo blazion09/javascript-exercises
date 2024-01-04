f4e3f36152f8b55549344af8dc60f6c3f3c31bbb
blazion09 <muhd09afiq@gmail.com>
2024-01-04 16:17:55 +0800

    chore findOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const peopleAge = people.map((age) => age.yearOfDeath - age.yearOfBirth);
+    let indexHighestAge = peopleAge.indexOf(Math.max(...peopleAge));
+    console.log(people[indexHighestAge]);
 };
+
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
