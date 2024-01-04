e7a1b34cbda11ea387f5b75280fae85796035a2d
blazion09 <muhd09afiq@gmail.com>
2024-01-04 13:40:18 +0800

    complete getTitle

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const n = books.length;
+    let bookTitle ='';
+    let allTitle = [];
+    for (let i = 0; i < n; i++){
+        bookTitle = books[i].title;
+        allTitle.push(bookTitle);
+    }
+    return allTitle;
 };
 
 // Do not edit below this line
